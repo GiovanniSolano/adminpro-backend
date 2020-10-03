47c06833fd788e6e98e5f3d9e4067cecbc787191
Giovanni Solano <giovannisolano211@gmail.com>
2020-10-03 11:08:51 -0500

    Modificación de path

--- routes/hospitales.js
+++ routes/hospitales.js
@@ -7,7 +7,7 @@ ruta: /api/hospitales
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { getHospitales, crearHospital, actualizarHospital, borrarHospital } = require('../controllers/Hospitales');
+const { getHospitales, crearHospital, actualizarHospital, borrarHospital } = require('../controllers/hospitales');
 const { validarJWT } = require('../middlewares/validar-jwt');
 const router = Router();
 
