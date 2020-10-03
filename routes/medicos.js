82eb6afb879b2080d7d0b382c002166329a2eba4
Giovanni Solano <giovannisolano211@gmail.com>
2020-10-03 11:14:04 -0500

    Rutas modificadas

--- routes/medicos.js
+++ routes/medicos.js
@@ -7,7 +7,7 @@ ruta: /api/medicos
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { getMedicos, getMedicoById, crearMedico, actualizarMedico, borrarMedico } = require('../controllers/Medicos');
+const { getMedicos, getMedicoById, crearMedico, actualizarMedico, borrarMedico } = require('../controllers/medicos');
 const { validarJWT } = require('../middlewares/validar-jwt');
 const router = Router();
 
